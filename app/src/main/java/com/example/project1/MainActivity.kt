121adade7bac62b98ed635ff40337fa4c1e75295
Thiennguyen21it <thiennh.21it@vku.udn.vn>
2023-01-06 11:12:46 +0700

    start kotlin project

---
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.project1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+}
